c3dd1ffa93c7a7282897d62ba3c85e1681fa426e
WhiteFish123 <bohe03554@gmail.com>
2026-07-19 17:54:08 +0800

    修复丢失修改

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
-
+using TMPro;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     // public float forceVal;
     public float moveSpeed=10f;
     public float extraGravityForce = 150f;
+    private int score = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public TextMeshProUGUI winText;
+    public TextMeshProUGUI scoreText;
     void Start()
     {
         rb=GetComponent<Rigidbody>();
@@ -31,5 +34,20 @@ public class PlayerController : MonoBehaviour
         // Vector3 moveDir=new Vector3(horizontal,0,vertical);
 
         // rb.AddForce(moveDir*forceVal);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.SetActive(false);
+        score++;
+        RefreshScoreText();
+        
+        if(score>=12)
+        {
+            winText.gameObject.SetActive(true); 
+        }
+    }
+    void RefreshScoreText()
+    {
+        scoreText.text="Score: "+score;
     }
 }
